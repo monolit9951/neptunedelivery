710eaad87754ac8820800754abc9e9cde2dff0f4
Ruslan_Bei <ruslan.bei@monobank.ua>
2024-02-28 12:28:18 +0200

    cross origins

--- src/main/java/com/gmail/merikbest2015/ecommerce/configuration/MvcConfiguration.java
+++ src/main/java/com/gmail/merikbest2015/ecommerce/configuration/MvcConfiguration.java
@@ -5,10 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class MvcConfiguration implements WebMvcConfigurer {
 
     @Value("${hostname}")
