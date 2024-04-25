b8851a37b9fc815c5a610000918b5634d704058d
Ruslan_Bei <rube@extern.inxmail.de>
2024-04-25 15:06:44 +0300

    fix

---
+++ src/main/resources/db/migration/V12__changeImage.sql
@@ -0,0 +1,2 @@
+UPDATE product SET img='https://i.ibb.co/M5dHgkd/793ad840-151f-4f1e-81f7-2c5dd56b552b.jpg';
+UPDATE product SET url='https://i.ibb.co/M5dHgkd/793ad840-151f-4f1e-81f7-2c5dd56b552b.jpg';
